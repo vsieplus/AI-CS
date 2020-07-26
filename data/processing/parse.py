eff98eef2804868c402951e037b294cb8ee37b05
vsieplus <ryansie@hotmail.com>
2020-07-26 00:54:20 -0700

    updated note, speed parsers + chart metadata creation

--- data/processing/parse.py
+++ data/processing/parse.py
@@ -3,10 +3,13 @@
 
 import logging
 import re
+import util
+
+from collections import OrderedDict
 
 int_parser = lambda x: int(x.strip()) if x.strip() else None
 bool_parser = lambda x: True if x.strip() == 'YES' else False
-str_parser = lambda x: x.strip() if x.strip() else None
+str_parser = lambda x: x.strip().lower() if x.strip() else None
 float_parser = lambda x: float(x.strip()) if x.strip() else None
 
 def unsupported_parser(attr_name):
@@ -62,33 +65,25 @@ def bpms_parser(x):
 
     return bpms_cleaned
 
-def stops_parser(x):
-    stops = list_parser(kv_parser(float_parser, float_parser))(x)
+# 'speeds' similar to bpms but diff format (beat=multiplier=duration=0, ...)
+def speeds_parser(x):
+    # really only need beat + speed multiplier
+    speeds = 
+        list_parser(kv_parser(kv_parser(kv_parser(float_parser, float_parser), float_parser), float_parser))(x)
 
-    beat_last = -1.0
-    for beat, stop_len in stops:
-        if beat == None or stop_len == None:
-            raise ValueError('Bad stop formatting')
-        if beat < 0.0:
-            raise ValueError('Bad beat in stop')
-        if stop_len == 0.0:
-            continue
-        if beat <= beat_last:
-            raise ValueError('Nonascending list of beats in stops')
-        beat_last = beat
-    return stops
+# represent notes as list of lists. sublist ~ measure, sub-elem ~ beat
+def notes_parser(x, chart_type = 'ssc'):
 
-def notes_parser(x):
-    pattern = r'([^:]*):' * 5 + r'([^;:]*)'
-    notes_split = re.findall(pattern, x)
+    if(chart_type not in ['ucs', 'ssc']):
+        return
+
+    pattern = r'([^;]*)'
+    notes = re.findall(pattern, x)
     if len(notes_split) != 1:
         raise ValueError('Bad formatting of notes section')
-    notes_split = notes_split[0]
-    if (len(notes_split) != 6):
-        raise ValueError('Bad formatting within notes section')
-
+    
     # parse/clean measures
-    measures = [measure.splitlines() for measure in notes_split[5].split(',')]
+    measures = [measure.splitlines() for measure in notes.split(',')]
     measures_clean = []
     for measure in measures:
         measure_clean = filter(lambda pulse: not pulse.strip().startswith('//') and len(pulse.strip()) > 0, measure)
@@ -100,22 +95,16 @@ def notes_parser(x):
     for measure in measures_clean:
         if len(measure) == 0:
             raise ValueError('Found measure with 0 notes')
-        if not len(measure) in VALID_PULSES:
-            parlog.warning('Nonstandard subdivision {} detected, allowing'.format(len(measure)))
 
-    chart_type = str_parser(notes_split[0])
-    if chart_type not in ['dance-single', 'dance-double', 'dance-couple', 'lights-cabinet']:
-        raise ValueError('Nonstandard chart type {} detected'.format(chart_type))
+    return measures_clean
 
-    return (str_parser(notes_split[0]),
-        str_parser(notes_split[1]),
-        str_parser(notes_split[2]),
-        int_parser(notes_split[3]),
-        list_parser(float_parser)(notes_split[4]),
-        measures_clean
-    )
+def ucs_notes_parser(x):
+    measures_clean = []
+
+    return measures_clean
 
 ATTR_NAME_TO_PARSER = {
+    # song attrs.
     'title': str_parser,
     'subtitle': str_parser,
     'artist': str_parser,
@@ -126,6 +115,10 @@ ATTR_NAME_TO_PARSER = {
     'credit': str_parser,
     'banner': str_parser,
     'background': str_parser,
+    'previewid': str_parser,
+    'jacket': str_parser,
+    'cdimage': str_parser,
+    'discimage': str_parser,
     'lyricspath': str_parser,
     'cdtitle': str_parser,
     'music': str_parser,
@@ -136,33 +129,53 @@ ATTR_NAME_TO_PARSER = {
     'songtype': str_parser,
     'songcategory': str_parser,
     'volume': int_parser,
-    'displaybpm': float_parser,
-    'bpms': bpms_parser,
+    'displaybpm': float_parser
     'stops': stops_parser,
     'delays': unsupported_parser('delays'),
     'warps': unsupported_parser('warps'),
     'timesignatures': list_parser(kv_parser(float_parser, kv_parser(int_parser, int_parser))),
     'tickcounts': list_parser(kv_parser(float_parser, int_parser)),
-    'combos': list_parser(kv_parser(float_parser, int_parser)),
-    'speeds': unsupported_parser('speeds'),
-    'scrolls': unsupported_parser('scrolls'),
-    'fakes': unsupported_parser('fakes'),
     'labels': list_parser(kv_parser(float_parser, str_parser)),
     'lastsecondhint': float_parser,
     'bgchanges': unsupported_parser('bgchanges'),
     'keysounds': str_parser,
     'attacks': str_parser,
     
-    'notes': unsupported_parser('notes'), # handle manually
+    # chart attrs.
+    'notedata': str_parser,
+    'chartname': str_parser,
+    'stepstype': str_parser,                    # 'pump-single'/'pump-double'
+    'description': str_parser,                  #  has 'oucs' distinction after chart type/level
+    'chartstyle': str_parser,
+    'difficulty': str_parser,                   # diff. as in basic mode (easy/med/hard/...)
+    'meter': int_parser,                        # difficulty level (numeric)
+    'radarvalues': list_parser(float_parser),
+    'credit': str_parser,                       # chart author (if known)
+    'patchinfo': str_parser,
+    'offset': float_parser,
 
+    # bpms used in chart stored as (beat=bpm, ...)
+    'bpms': bpms_parser,
 
+    'combos': list_parser(kv_parser(float_parser, float_parser)),
+    'speeds': speeds_parser,
+    
+    # scroll speeds for holds?
+    'scrolls': list_parser(kv_parser(float_parser, float_parser)),
+
+    'fakes': unsupported_parser('fakes'),
+    'notes': unsupported_parser('notes'),       # handle manually
 }
 
-# attributes with potentially multiple sets (for each of the chart types)
-ATTR_MULTI = ['notedata', 'chartname', 'stepstype', 'description', 'chartstyle',
+# distinguish song and chart attributes
+ATTR_SONG = []
+ATTR_CHART = ['notedata', 'chartname', 'stepstype', 'description', 'chartstyle',
     'difficulty', 'meter', 'radarvalues', 'credit', 'patchinfo', 'offset', 
-    'bpms', 'stops', 'delays', 'warps', 'timesignatures', 'tickcounts', 'combos',
-    'speeds', 'scrolls', 'fakes', 'labels', 'notes']
+    'bpms', 'tickcounts', 'combos', 'speeds', 'scrolls', 'fakes', 'labels', 'notes']
+ATTR_CHART_REQUIRED = ['stepstype', 'description', 'meter', 'credit', 'offset',
+    'bpms', 'speeds', 'notes']
+
+# attributes with potentially multiple sets (for each of the chart types)
 ATTR_NOTES = 'notes'
 
 def parse_chart_txt(chart_txt, chart_type):
@@ -178,22 +191,49 @@ def parse_chart_txt(chart_txt, chart_type):
 
         # if attribute is 'notes', parse differently depending on chart type
         if attr_name == ATTR_NOTES:
-            if chart_type == 'ucs':
-                attr_val_parsed = ucs_parser(attr_val)
-            elif chart_type == 'ssc':
-                attr_val_parsed = ssc_parser(attr_val)
-            else:
-                raise NotImplementedError('Chart type {} invalid'.format(chart_type))
+            attr_val_parsed = notes_parser(attr_val, chart_type)
         else:
             attr_val_parsed = ATTR_NAME_TO_PARSER[attr_name](attr_val)
-        
+
+        # check for duplicate song attrs
         if attr_name in attrs:
-            if attr_name not in ATTR_MULTI:
-                if attr_val_parsed == attrs[attr_name]:
-                    continue
+            if attr_val_parsed == attrs[attr_name]:
+                continue
+            else:
+                raise ValueError('Song Attribute {} defined multiple times'.format(attr_name))
+        elif attr_name in ATTR_CHART:
+            if attr_name not in ATTR_CHART_REQUIRED:
+                continue
+
+            if 'charts' in attrs:
+                latest_chart = attrs['charts'][-1]
+                latest_chart_attr = next(reversed(latest_chart))
+
+                # check if last chart finished
+                if latest_chart_attr = 'notes':
+                    # start new chart
+                    next_chart = OrderedDict([(attr_name, attr_val_parsed)])
+                    attrs['charts'].append(next_chart)
                 else:
-                    raise ValueError('Attribute {} defined multiple times'.format(attr_name))
-            attrs[attr_name].append(attr_val_parsed)
+                    # skip if a UCS chart in an ssc
+                    if attr_name == 'description' and len(re.findall('ucs', attr_val_parsed)) > 0:
+                        del attrs['charts'][-1]
+                        continue
+
+                    # if notes,  convert -> ([measure, beat, split], abs_beat,
+                    # abs_time, notes [as str ~ '1001'])
+                    if attr_name == 'notes':
+                        attr_val_parsed = util.calc_note_beats_and_abs_times(
+                            latest_chart['offset'], latest_chart['bpms'],
+                            attr_val_parsed)
+                    
+                    latest_chart[attr_name] = attr_val_parsed
+            else:                
+                attrs['charts'] = []
+
+                first_chart = OrderedDict([(attr_name, attr_val_parsed)])
+                attrs['charts'].append(first_chart)
+
         else:
             attrs[attr_name] = attr_val_parsed
 
