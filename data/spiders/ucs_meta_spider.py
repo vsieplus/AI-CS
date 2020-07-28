3a553f2b906494374673715bb07c71cfacb92d74
vsieplus <ryansie@hotmail.com>
2020-07-27 20:57:07 -0700

    separated crawlers for downloading/gathering metadata

--- data/spiders/ucs_meta_spider.py
+++ data/spiders/ucs_meta_spider.py
@@ -1,7 +1,17 @@
-# scrape ucs metada from
+# scrape ucs metadatas
 
 import scrapy
+from scrapy.pipelines.files import FilesPipeline
+
 import re
+import os
+from pathlib import Path
+
+ABS_PATH = Path(__file__).parent.absolute()
+UCS_BASE_DATA_PATH = os.path.join(str(ABS_PATH), '../dataset/raw/00-UCS_BASE')
+
+if not os.path.isdir(UCS_BASE_DATA_PATH):
+    os.makedirs(UCS_BASE_DATA_PATH)
 
 VERSION_PATTERN = re.compile('version_([0-9]+)')
 VERSION_DICT = {
@@ -11,10 +21,42 @@ VERSION_DICT = {
     14: 'nx2', 15: 'nx_absolute', 16: 'fiesta', 17: 'fiesta_ex', 
     18: 'fiesta2', 19: 'prime', 20: 'prime2', 21: 'xx'}
 
+# crawl ucs file downloads
+class UCS_DownloadSpider(scrapy.Spider):
+    name = 'ucs_download'
+    start_urls = ['http://www.piugame.com/piu.ucs/ucs.sample/ucs.sample.alltunes.php']
+       
+    custom_settings = {
+        'ITEM_PIPELINES': {'spiders.ucs_meta_spider.UCSZipPipeline': 1},
+        'FILES_STORE': UCS_BASE_DATA_PATH,
+        'MEDIA_ALLOW_REDIRECTS': True
+    }
+
+    def parse(self, response):
+        ucs_rows = response.xpath('//tr')
+        ucs_codes = ucs_rows.xpath('.//span[@class="download_cs_number"]/text()').getall()
+        
+        # download/unzip ucs zip files
+        dl_links = ucs_rows.xpath('.//td[@class="download_source"]').xpath('.//a/@href').getall()
+        assert(len(ucs_codes) == len(dl_links))
+
+        for i, dl_link in enumerate(dl_links):
+            yield {'file_urls': [response.urljoin(dl_link)], 'name': ucs_codes[i]}
+
+        page_area = response.xpath('//span[@class="pg"]')
+
+        curr_page = int(page_area.xpath('.//strong[@class="pg_current"]/text()').get())
+        next_page = curr_page + 1
+        next_page_link = page_area.xpath('.//a[contains(@href, "' + str(next_page) + '")]/@href').get()
+
+        if next_page_link:
+            yield response.follow(next_page_link, callback=self.parse)
+
+# crawl ucs metadata
 class UCS_MetaSpider(scrapy.Spider):
     name = 'ucs_meta'
     start_urls = ['http://www.piugame.com/piu.ucs/ucs.sample/ucs.sample.alltunes.php']
-
+    
     def parse(self, response):
         # //tr for each row corresponding to a ucs
         ucs_rows = response.xpath('//tr')
@@ -32,6 +74,7 @@ class UCS_MetaSpider(scrapy.Spider):
         assert(len(versions) == len(bpms) and len(bpms) == len(song_artists) and
             len(song_artists) == len(song_names) and len(song_names) == len(ucs_codes))
 
+        # add ucs data to ucs_dict
         if 'ucs_dict' in response.meta:
             ucs_dict = response.meta['ucs_dict']
         else:
@@ -45,10 +88,11 @@ class UCS_MetaSpider(scrapy.Spider):
             ucs_meta['version'] = versions[i]
 
             ucs_dict[code] = ucs_meta
-        
+
+        # check for next page
         page_area = response.xpath('//span[@class="pg"]')
 
-        curr_page = int(response.xpath('.//strong[@class="pg_current"]/text()').get())
+        curr_page = int(page_area.xpath('.//strong[@class="pg_current"]/text()').get())
         next_page = curr_page + 1
         next_page_link = page_area.xpath('.//a[contains(@href, "' + str(next_page) + '")]/@href').get()
 
@@ -56,12 +100,27 @@ class UCS_MetaSpider(scrapy.Spider):
             yield response.follow(next_page_link, callback=self.parse, 
                 meta={**(response.meta), **{'ucs_dict': ucs_dict}})
         else:
-            yield ucs_dict
+            yield ucs_dict      
+
+class UCSZipPipeline(FilesPipeline):    
+    def file_path(self, request, response=None, info=None):
+        return request.meta.get('filename','') + '.zip'
+
+    def get_media_requests(self, item, info):
+        file_url = item['file_urls'][0]
+        meta = {'filename': item['name']}
+        yield scrapy.Request(url=file_url, meta=meta)
+    
+    # unzip upon download
+    def item_completed(self, results, item, info):
+        file_paths = [file['path'] for ok, file in results if ok]
+
+        for fp in file_paths:
+            fp_abs = os.path.join(UCS_BASE_DATA_PATH, fp)
+
+            out_path = re.sub('.zip', '', fp_abs)
             
+            os.system('unzip -d ' + out_path + ' ' + fp_abs)
+            os.system('rm -f ' + fp_abs)
 
-            
-            
-
-
-
-
+        return item
--- data/ucs_add_metadata.py
+++ data/ucs_add_metadata.py
@@ -11,24 +11,49 @@ from scrapy.crawler import CrawlerProcess
 
 ABS_PATH = pathlib.Path(__file__).parent.absolute()
 CRAWLER_PATH = os.path.join(str(ABS_PATH), 'spiders/ucs_meta_spider.py')
-META_JSON_PATH = os.path.join(str(ABS_PATH), 'ucs_metadata.json')
+META_JSON_PATH = os.path.join(str(ABS_PATH), 'dataset/00_ucs_metadata.json')
 
 def add_metadata(ucs_dir, ucs_metadata):
     ucs_fps = glob.glob(os.path.join(ucs_dir, '*.ucs'))
 
     for ucs_fp in ucs_fps:
-        ucs_id = ucs_fp.split('.')[0]
+        ucs_id = os.path.split(ucs_fp)[-1].split('.')[0]
 
         try:
-            ucs_meta = UCS_METADATA[ucs_id]
+            ucs_meta = ucs_metadata[ucs_id]
         except KeyError:
-            print('UCS code {} not found. Please enter additional info:'.format(ucs_id))
+            print('UCS code {} not found. Please enter additional info. '.format(ucs_id))
+            ucs_meta = {}
+            
+            ucs_meta['title'] = input('Song name: ')
+            ucs_meta['artist'] = input('Artist: ')
+            ucs_meta['bpms'] = input('BPM: ')
+            ucs_meta['version'] = input('Version debuted: ')
+            
+        # prompt user for other + chart-specific info
+        print('Please enter the following chart information')
+        ucs_meta['songtype'] = input('Song Type [arcade, remix, fullsong, shortcut]: ')
+        ucs_meta['genre'] = input('Genre [k-pop, original, world_music, j-music, xross]: ')
+        ucs_meta['author'] = input('Chart author: ')
+        ucs_meta['chart_type'] = input('Chart type ["pump-single"/"pump-double"]:')
+        ucs_meta['meter'] = input('Chart level: ')
+
+        ucs_meta_str = ''
+        for k,v in ucs_meta.items():
+            ucs_meta_str += ':{}={}\n'.format(k, v)
+
+        with open(ucs_fp, 'r+') as f:
+            old = f.read()
+            f.seek(0, 0)
+            f.write(ucs_meta_str + old)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--ucs_dir', type=str, help='directory with base .ucs files', required=True)
     parser.add_argument('--scrape_meta', action='store_true', default=False, 
         help='scrape generic ucs metadata')
+    parser.add_argument('--download', action='store_true', default=False,
+        help='download UCS files from the site')
 
     args = parser.parse_args()
 
@@ -43,6 +68,11 @@ def main():
         })
 
         process.crawl(ucs_meta_spider.UCS_MetaSpider)
+        process.start()
+
+    if args.download or not os.path.isdir(ucs_meta_spider.UCS_BASE_DATA_PATH):
+        process = CrawlerProcess()
+        process.crawl(ucs_meta_spider.UCS_DownloadSpider)
         process.start()
 
     with open(META_JSON_PATH, 'r') as f:
