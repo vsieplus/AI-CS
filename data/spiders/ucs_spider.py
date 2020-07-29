eb52cc8a4e45bfab89a8e26340fbfa08ce6f84ee
vsieplus <ryansie@hotmail.com>
2020-07-28 18:24:56 -0700

    implemented initial ucs spider

--- data/spiders/ucs_spider.py
+++ data/spiders/ucs_spider.py
@@ -5,16 +5,20 @@ from scrapy.pipelines.files import FilesPipeline
 
 import re
 import os
+import json
 import datetime
 from pathlib import Path
+
+from spiders.ucs_meta_spider import UCS_BASE_DATA_PATH
 
 UCS_STEPARTIST_URL = 'http://www.piugame.com/bbs/board.php?ucs_event_no=&bo_table=ucs&sca=&sop=and&sfl=wr_name%2C1&stx='
 UCS_SONG_URL = 'http://www.piugame.com/bbs/board.php?ucs_event_no=&bo_table=ucs&sca=&sop=and&sfl=ucs_song_no%2C1&stx='
 
-ABS_PATH = pathlib.Path(__file__).parent.absolute()
-OUT_DIR = os.path.join(str(ABS_PATH), 'dataset/raw/')
+ABS_PATH = Path(__file__).parent.absolute()
+OUT_DIR = os.path.join(str(ABS_PATH), '../dataset/raw/')
 
 UCS_LVL_PATTERN = 's_lv([0-9][0-9])'
+UCS_CODE_PATTERN = '_(CS[0-9][0-9][0-9])#scrollSpeed='
 
 class UCS_Spider(scrapy.Spider):
     name = 'ucs_spider'
@@ -27,9 +31,13 @@ class UCS_Spider(scrapy.Spider):
 
     # specific > generic (step_artist > song > level > mix > ...)
     def __init__(self, pack_name, step_artists, min_level, max_level,
-        chart_type, songs, start_date, end_date):
+        chart_type, songs, start_date, end_date, meta_json_path):
 
         self.pack_name = pack_name
+        outpath = os.path.join(OUT_DIR, pack_name)
+        if not os.path.isdir(outpath):
+            os.makedirs(outpath)
+
         self.min_level = min_level
         self.max_level = max_level
         self.chart_type = chart_type
@@ -38,11 +46,14 @@ class UCS_Spider(scrapy.Spider):
         self.start_date = start_date
         self.end_date = end_date
 
+        with open(meta_json_path, 'r') as f:
+            self.UCS_METADATA = json.loads(f.read())[0]
+
         assert(start_date < end_date)
 
         # if step artists/songs provided only start from their pages
         if step_artists:
-            self.start_urls = [UCS_STEPARTIST_URL + step_artist for artist in step_artists]
+            self.start_urls = [UCS_STEPARTIST_URL + artist for artist in step_artists]
             # may still have additional song restrictions
         elif songs:
             self.start_urls = [UCS_SONG_URL + song_title for song_title in songs]
@@ -85,17 +96,26 @@ class UCS_Spider(scrapy.Spider):
             valid_level = self.min_level <= chart_level and chart_level <= self.max_level
 
             if correct_chart_type and valid_level:
-                ucs_page_url = response.xpath('.//span[@class="share_song_title"]/a/@href').get()
-                yield response.follow(ucs_page_url, callback=self.parse_ucs_page,
-                    meta={'chart_type': self.chart_type, 'level': chart_level})
+                stepmaker = response.xpath('.//td[@class="share_stepmaker"]/img/text()')
+                ucs_code = re.search(UCS_CODE_PATTERN, 
+                    response.xpath('.//a[@class="btnUCSPlayr share_download1"]/@rel').get()).group(1)
+                
+                dl_link = response.xpath('.//a[@class="share_download2"]/@href')
 
-    def parse_ucs_page(self, response):
-        a = 1
+                # login before download
+
+                this_ucs_meta = self.UCS_METADATA[ucs_code]
+
+                yield {**this_ucs_meta, **{'file_urls': [response.urljoin(dl_link)], 
+                    'name': ucs_code, 'step_artist': stepmaker, 
+                    'chart_type': self.chart_type, 'meter': self.chart_level,
+                    'songtype': 'arcade', 'pack_name': self.pack_name}}
+
 
 # custom pipeline for ucs files
-class UCSZipPipeline(FilesPipeline):
+class UCSPipeline(FilesPipeline):
     def file_path(self, request, response=None, info=None):
-        return request.meta.get('filename','')
+        return request.meta.get('filename','') + '.ucs'
 
     def get_media_requests(self, item, info):
         file_url = item['file_urls'][0]
@@ -107,11 +127,26 @@ class UCSZipPipeline(FilesPipeline):
         file_paths = [file['path'] for ok, file in results if ok]
 
         for fp in file_paths:
-            fp_abs = os.path.join(UCS_BASE_DATA_PATH, fp)
+            # add metadata to file
+            fp_abs = os.path.join(OUT_DIR, fp)
+            add_metadata(item, fp_abs)
 
-            out_path = re.sub('.zip', '', fp_abs)
-            
-            os.system('unzip -o -d ' + out_path + ' ' + fp_abs)
-            os.system('rm -f ' + fp_abs)
+            # create + move to subfolder
+            subfolder = os.path.join(OUT_DIR, item['pack_name'], item['title'])
+            subfolder = os.path.join(subfolder, item['chart_type'] + item['meter']
+                + '_' + item['step_artist'])
+            os.makedirs(subfolder)
+
+            os.system('mv {} {}'.format(fp_abs, subfolder))
 
         return item
+
+    def add_metadata(self, item, fp):
+        ucs_meta = ''
+        for k,v in item.items():
+            ucs_meta += ':{}={}'.format(k,v)
+
+        with open(fp, 'r+') as f:
+            old = f.read()
+            f.seek(0,0)
+            f.write(ucs_meta + old)
--- data/ucs_scrape.py
+++ data/ucs_scrape.py
@@ -5,6 +5,7 @@ import argparse
 import datetime
 
 from spiders import ucs_spider, ucs_meta_spider
+from ucs_add_metadata import META_JSON_PATH, crawl_base_download, crawl_meta_download
 from scrapy.crawler import CrawlerProcess
 
 def parse_args() -> argparse.Namespace:
@@ -15,38 +16,40 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument('--max_level', type=int, help='minimum difficulty level', default=28)
     parser.add_argument('--chart_type', type=str, choices=['single', 'double', 'coop',
         'douper', 'sinper'], help='type of chart')
-    parser.add_argument('--songs', type-str, nargs='+', help='restrict to these songs', default=None)
-    parser.add_argument('--start_date', type=str, help='restrict to ucs made since (MM/DD/YYYY)',
+    parser.add_argument('--songs', type=str, nargs='+', help='restrict to these songs', default=None)
+    parser.add_argument('--start_date', type=str, help='restrict to ucs made since (YYYY-MM-DD)',
         default=datetime.date(year=2015, month=1, day=1))
-    parser.add_argument('--end_date', type=str, help='restrict to ucs made up to (MM/DD/YYYY)',
+    parser.add_argument('--end_date', type=str, help='restrict to ucs made up to (YYYY-MM-DD)',
         default=datetime.date.today())
 
     return parser.parse_args()
 
-# format (mm/dd/yyyy)
-def txt_to_date(text):
-    date = [int(number) for number in text.split('/')]
-    return datetime.date(month=date[0], day=date[1], year=date[2])
+def get_date(date_arg):
+    if isinstance(date_arg, datetime.date):
+        return date_arg
+    else:
+        return datetime.date.fromisoformat(date_arg)
 
 def main():
     args = parse_args()
 
-    custom_pack_name = input('Please name this ucs pack:')
+    custom_pack_name = input('Please name this ucs pack: ')
     process = CrawlerProcess()
 
-    # download base ucs if not yet already
+    # download base ucs/metadata if not yet already
     if not os.path.isdir(ucs_meta_spider.UCS_BASE_DATA_PATH):
-        process = CrawlerProcess()
-        process.crawl(ucs_meta_spider.UCS_DownloadSpider)
-        process.start()
+        crawl_base_download()
+    
+    if not os.path.isfile(META_JSON_PATH):
+        crawl_meta_download()
 
-    start_date_obj = txt_to_date(args.start_date)
-    end_date_obj = txt_to_date(args.end_date)
+    start_date_obj = get_date(args.start_date)
+    end_date_obj = get_date(args.end_date)
 
-    process.crawl(ucs_meta_spider.UCS_Spider, pack_name=custom_pack_name,
+    process.crawl(ucs_spider.UCS_Spider, pack_name=custom_pack_name,
         step_artists=args.step_artists, min_level=args.min_level,
-        max_level=args.max_level, chart_type=args.chart_type, songs=args.songs
-        start_date=start_date_obj, end_date=end_date_obj)        
+        max_level=args.max_level, chart_type=args.chart_type, songs=args.songs,
+        start_date=start_date_obj, end_date=end_date_obj, meta_json_path=META_JSON_PATH)        
     process.start()
 
 if __name__ == '__main__':
