af06f8bf4547fb665df4b0a3d1943f85aec6f361
vsieplus <ryansie@hotmail.com>
2020-07-29 19:02:55 -0700

    experimenting with python requests for ucs login

--- data/spiders/ucs_spider.py
+++ data/spiders/ucs_spider.py
@@ -2,9 +2,12 @@
 
 import scrapy
 from scrapy.pipelines.files import FilesPipeline
+from lxml import html
+from lxml.etree import tostring
 
 import re
 import os
+import requests
 import json
 import datetime
 from pathlib import Path
@@ -21,6 +24,7 @@ from util import ez_name
 UCS_STEPARTIST_URL = 'http://www.piugame.com/bbs/board.php?ucs_event_no=&bo_table=ucs&sca=&sop=and&sfl=wr_name%2C1&stx='
 UCS_SONG_URL = 'http://www.piugame.com/bbs/board.php?ucs_event_no=&bo_table=ucs&sca=&sop=and&sfl=ucs_song_no%2C1&stx='
 BASE_DL_URL = 'http://piugame.com/bbs/'
+BASE_LOGIN_URL = 'https://www.piugame.com/bbs/piu.login_check.php'
 
 OUT_DIR = os.path.join(str(ABS_PATH), '../dataset/raw/')
 
@@ -41,9 +45,6 @@ class UCS_Spider(scrapy.Spider):
         chart_type, songs, start_date, end_date, meta_json_path):
 
         self.pack_name = pack_name
-        outpath = os.path.join(OUT_DIR, pack_name)
-        if not os.path.isdir(outpath):
-            os.makedirs(outpath)
 
         self.min_level = min_level
         self.max_level = max_level
@@ -65,45 +66,52 @@ class UCS_Spider(scrapy.Spider):
         elif songs:
             self.start_urls = [UCS_SONG_URL + song_title for song_title in songs]
         else:
-            start_urls = ['http://www.piugame.com/bbs/board.php?bo_table=ucs']
+            start_urls = [BASE_LOGIN_URL]
         
+        # login to ucs site to enable downloads
         print('Please enter login credentials: ')
-        self.user_email = input('User Email: ')
-        self.user_pw = getpass('Password: ')
-    
+        user_email = input('User Email: ')
+        user_pw = getpass('Password: ')
+
+        payload = {'mb_id': user_email, 'mb_password': user_pw}
+
+        self.session_requests = requests.session()
+        result = self.session_requests.post(BASE_LOGIN_URL, data=payload,
+            headers={'referer': BASE_LOGIN_URL})
+        print(result.text)
+
     def verify_login(self, response):
         return
-        #if 'wrong Password' in response.xpath('//').get():
+        #if 'wrong Password' in response.xpath('//')[0]:
         #    self.logger.error('Login failed')
 
     def parse(self, response):
         # check if need to login or already logged in
         # http://scrapingauthority.com/2016/11/22/scrapy-login/
-        login_area = response.xpath('//form[@name="foutlogin"]')
+        # login_area = response.xpath('//form[@name="foutlogin"]')
 
-        if login_area:
-            yield scrapy.FormRequest.from_response(response,
-                formdata={'text': self.user_email, 'password': self.user_pw},
-                callback=self.verify_login)
+        # if login_area:
+        #     yield scrapy.FormRequest.from_response(response,
+        #         formdata={'mb_id': self.user_email, 'mb_password': self.user_pw},
+        #         callback=self.verify_login)
+        result = self.session_requests.get(response.url, headers={'referer': response.url})
+        new_response = html.fromstring(result.content)
 
-        ucs_rows = response.xpath('//div[@class="share_list"]/table/tbody/tr')
+        ucs_rows = new_response.xpath('//div[@class="share_list"]/table/tbody/tr')
 
         for ucs_row in ucs_rows:
             for download in self.parse_ucs_row(ucs_row):
                 yield download
 
-        page_area = response.xpath('//span[@class="pg"]')
+        page_area = new_response.xpath('//span[@class="pg"]')[0]
 
-        curr_page = int(page_area.xpath('.//strong[@class="pg_current"]/text()').get())
+        curr_page = int(page_area.xpath('.//strong[@class="pg_current"]/text()')[0])
         next_page = curr_page + 1
-        next_page_link = page_area.xpath('.//a[contains(@href, "page=' + str(next_page) + '")]/@href').get()
-
-        if next_page_link:
-            yield response.follow(next_page_link, callback=self.parse)
+        next_page_link = page_area.xpath('.//a[contains(@href, "page=' + str(next_page) + '")]/@href')
 
     def parse_ucs_row(self, response):
         # date in (YY-MM-DD) format
-        upload_date = response.xpath('.//td[@class="share_upload_date"]/text()').get().split('-')
+        upload_date = response.xpath('.//td[@class="share_upload_date"]/text()')[0].split('-')
         upload_date_obj = datetime.date(year=int('20' + upload_date[0]),
             month=int(upload_date[1]), day=int(upload_date[2]))
         correct_date = self.start_date < upload_date_obj and upload_date_obj < self.end_date
@@ -111,20 +119,20 @@ class UCS_Spider(scrapy.Spider):
         if not correct_date:
             return
 
-        song_title = response.xpath('.//span[@class="share_song_title"]/a/text()').get().strip().lower()
+        song_title = response.xpath('.//span[@class="share_song_title"]/a/text()')[0].strip().lower()
 
         if not self.songs or self.songs and song_title in self.songs:
-            chart_leveltype = response.xpath('.//td[@class="share_level"]/span/@class').get()
+            chart_leveltype = response.xpath('.//td[@class="share_level"]/span/@class')[0]
             chart_level = int(re.search(UCS_LVL_PATTERN, chart_leveltype).group(1))
 
             correct_chart_type = self.chart_type in chart_leveltype 
             valid_level = self.min_level <= chart_level and chart_level <= self.max_level
 
             if correct_chart_type and valid_level:
-                stepmaker = response.xpath('.//td[@class="share_stepmaker"]/text()').get().strip()
-                ucs_code = re.search(UCS_CODE_PATTERN, response.get()).group(1)
+                stepmaker = response.xpath('.//td[@class="share_stepmaker"]/text()')[0].strip()
+                ucs_code = re.search(UCS_CODE_PATTERN, tostring(response, encoding='unicode')).group(1)
                 
-                dl_link = response.xpath('.//a[@class="share_download2"]/@href').get()
+                dl_link = response.xpath('.//a[@class="share_download2"]/@href')[0]
 
                 this_ucs_meta = self.UCS_METADATA[ucs_code]
 
