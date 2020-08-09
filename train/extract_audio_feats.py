00326be80a7226c05eecc20c07176bfe5c2f8077
vsieplus <ryansie@hotmail.com>
2020-08-08 22:50:55 -0700

    refactor load_audio into -> extract_audio_feats

--- train/extract_audio_feats.py
+++ train/extract_audio_feats.py
@@ -19,11 +19,9 @@ def normalize_features(features):
 
     return (features - mean) / sd
 
-
-# waveform shape: [channel, time]
 # n_ffts -> sizes of FFT to use; default ~ 23ms, 46ms, 93ms
-def extract_audio_feats(waveform, sample_rate, n_ffts=[1024, 2048, 4096], 
-    hop_length=512, pad=0, n_mels=80):
+def extract_audio_feats(audio_fp, n_ffts=[1024, 2048, 4096], hop_length=512, pad=0, n_mels=80):
+    waveform, sample_rate = load_audio(audio_fp)
 
     audio_feats = []
     
