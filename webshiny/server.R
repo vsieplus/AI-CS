550abe4eba1ddb208eae67567b0cf8c5934d0950
vsieplus <ryansie@hotmail.com>
2020-08-11 23:40:29 -0700

    added ability to generate/play audio clip

--- webshiny/server.R
+++ webshiny/server.R
@@ -1,18 +1,39 @@
 # shiny app logic
 
-# load libraries
+# load libraries/functions
 library(shiny)
-library(ggplot2)
-library(reticulate) # https://rstudio.github.io/reticulate/
 
-CHART_LEVELS = list('pump-single' = 26, 'pump-double' = 28)
+source('generate.R', local = TRUE)
+source('visualize.R', local = TRUE)
 
 server <- function(input, output, session) {
   
-  # update level slider
+  
+  # produce spectrogram plot for new audio file
+  output$spectrogram_plot <- renderPlot({
+    req(input$audio_file)
+    plotSpectrogram(input$audio_file$datapath)
+  }, height = 400, width = 600)
+  
+  # play audio if an audio file has been uploaded + user clicked play
+  observeEvent(input$play_audio, {
+    req(input$audio_file)
+    output$audio <- renderUI(tags$audio(src = 'clip.wav', type = 'audio/wav',
+                             autoplay = NA, controls = NA,
+                             style = 'display: none;'))
+  })
+  
+  # update model summary
+  output$model_summary <- renderText({
+    getModelSummary(input$model)
+  })
+  
+  # update level slider and model options based on chart type
   observeEvent(input$chart_type, {
-    n_levels = CHART_LEVELS[[input$chart_type]]
+    n_levels <- CHART_LEVELS[[input$chart_type]]
     updateSliderInput(session, 'chart_level', max = n_levels)
+    
+    updateSelectInput(session, 'model', choices = modelsList[[input$chart_type]])
   })
 }
 
--- webshiny/visualize.R
+++ webshiny/visualize.R
@@ -6,8 +6,7 @@ library(av)
 samplePath = file.path('www', 'clip.wav')
 
 plotSpectrogram <- function(audio_path) {
-  fft_data <- av::read_audio_fft(audio_path, overlap = 0.5,
-                                 start_time = 45.0, end_time = 55.0)
+  fft_data <- av::read_audio_fft(audio_path, start_time = 45.0, end_time = 55.0)
   # save sample clip for output
   av::av_audio_convert(audio_path, samplePath, start_time = 45.0, total_time = 10.0)
   
