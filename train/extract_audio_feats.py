fixed audio feature normalization

train/extract_audio_feats.py
@@ -12,12 +12,15 @@ def load_audio(audio_fp):
 
     return waveform, sample_rate
 
-# apply standard normalization to the given tensor
+# apply normalization to the given features tensor [timestep, channel]
 def normalize_features(features):
     mean = features.mean()
     sd = features.std()
 
-    return (features - mean) / sd
+    if sd == 0:
+        sd = 1
+
+    return (features - features.mean()) / features.std()
 
 # n_ffts -> sizes of FFT to use; default ~ 23ms, 46ms, 93ms
 def extract_audio_feats(waveform, sample_rate, n_ffts=[1024, 2048, 4096], hop_length=512, pad=0, n_mels=80):
@@ -29,21 +32,19 @@ def extract_audio_feats(waveform, sample_rate, n_ffts=[1024, 2048, 4096], hop_le
             n_fft=n_fft, pad=pad, n_mels=n_mels, hop_length=hop_length)(waveform)
         mel_specgram = mel_specgram.squeeze(0)
 
-        # scale outputs logarithmically [avoid -inf values by doing abs + adding 1e-16]
-        log_mel_specgram = torch.log(torch.abs(mel_specgram) + 1e-16)
+        # scale outputs logarithmically [avoid -inf values by doing abs + adding 1e-6]
+        log_mel_specgram = torch.log(torch.abs(mel_specgram) + 1e-6)
 
-        audio_feats.append(mel_specgram)
+        audio_feats.append(log_mel_specgram)
     
-    # shape: [n_mels, time, len(n_ffts)] ~ default: [80, ?, 3] -> [?, 80, 3] (post-transpose)
+    # shape: [n_mels, time, len(n_ffts)] ~ default: [80, ?, 3]
     audio_feats = torch.stack(audio_feats, dim=-1)
-    audio_feats = audio_feats.transpose(0, 1)
 
     # standard normalization across frequency bands
-    for timestep in range(audio_feats.size(0)):
-        for freq_band in range(audio_feats.size(1)):
-            audio_feats[timestep, freq_band] = normalize_features(audio_feats[timestep, freq_band])
+    for freq_band in range(audio_feats.size(0)):
+        audio_feats[freq_band] = normalize_features(audio_feats[freq_band])
 
-    # transpose -> final shape: [3, ?, 80]
-    audio_feats = audio_feats.transpose(0, 2).transpose(1, 2)
+    # transpose -> final shape: [3, ?, 80] (channel, timestep, frequency)
+    audio_feats = audio_feats.transpose(0, 2)
 
     return audio_feats

train/hyper.py
@@ -4,7 +4,7 @@ from torch.nn import CrossEntropyLoss
 
 SEED = 1949
 
-PAD_IDX = -50
+PAD_IDX = -1
 
 N_CHART_TYPES = 2
 N_LEVELS = 28
@@ -13,7 +13,7 @@ CHART_FRAME_RATE = 100 # 10 ms per (chart) frame
 BATCH_SIZE = 1
 HIDDEN_SIZE = 128
 NUM_EPOCHS = 25
-MAX_GRAD_NORM = 2
+MAX_GRAD_NORM = 5
 
 HOP_LENGTH = 512
 
@@ -29,7 +29,7 @@ PLACEMENT_KERNEL_SIZES = [(7, 3), (3, 3)]   # (time, frequency) [height, width]
 PLACEMENT_POOL_STRIDE = (1, 3)
 PLACEMENT_POOL_KERNEL = (1, 3)
 
-PLACEMENT_UNROLLING_LEN = 100       # 100 frames of unrolling for placement model
+PLACEMENT_UNROLLING_LEN = 100
 
 # Thresholds for peak picking
 MIN_THRESHOLD = 0.25
@@ -41,7 +41,7 @@ PLACEMENT_INPUT_SIZE = N_CHART_TYPES + N_LEVELS + 160
 NUM_PLACEMENT_LSTM_LAYERS = 2
 
 SELECTION_CRITERION = CrossEntropyLoss(ignore_index=PAD_IDX)
-SELECTION_LR = 0.001
+SELECTION_LR = 0.0001
 
 SELECTION_HIDDEN_WEIGHT = 0.7
 NUM_SELECTION_LSTM_LAYERS = 2

train/train_rnns.py
@@ -141,7 +141,7 @@ def get_sequence_lengths(curr_unrolling, sequence_unroll_lengths, unroll_length)
     
     return sequence_lengths
 
-def run_placement_batch(cnn, rnn, placement_params, optimizer, criterion, batch, 
+def run_placement_batch(cnn, rnn, optimizer, criterion, batch, 
                         device, writer, do_train, curr_train_epoch=0):
     """train or eval the placement models with the specified parameters on the given batch"""
     if do_train:
@@ -229,14 +229,14 @@ def run_placement_batch(cnn, rnn, placement_params, optimizer, criterion, batch,
         if do_train:
             loss.backward()
             
-            # clip grads before step if L2 norm > 5
-            nn.utils.clip_grad_norm_(placement_params, max_norm=MAX_GRAD_NORM)
+            # clip grads before step
+            nn.utils.clip_grad_norm_(cnn.parameters(), max_norm=MAX_GRAD_NORM)
+            nn.utils.clip_grad_norm_(rnn.parameters(), max_norm=MAX_GRAD_NORM)
             optimizer.step()
             optimizer.zero_grad()
         
         predictions = predict_placements(logits, levels, audio_lengths)
         total_accuracy += get_placement_accuracy(predictions, targets, audio_lengths)
-
         total_loss += loss.item()
 
     clstm_hiddens = [torch.cat(hiddens_seq, dim=0) for hiddens_seq in clstm_hiddens]
@@ -338,7 +338,7 @@ def run_selection_batch(rnn, optimizer, criterion, batch, device, clstm_hiddens,
 
     return total_loss / num_unrollings, total_accuracy / num_unrollings
 
-def evaluate(p_cnn, p_rnn, p_params, s_rnn, data_iter, p_criterion, s_criterion,
+def evaluate(p_cnn, p_rnn, s_rnn, data_iter, p_criterion, s_criterion,
              device, writer, curr_validation):
     total_p_loss = 0
     total_s_loss = 0
@@ -347,7 +347,7 @@ def evaluate(p_cnn, p_rnn, p_params, s_rnn, data_iter, p_criterion, s_criterion,
 
     with torch.no_grad():
        for i, batch in enumerate(data_iter):
-            p_loss, p_acc, hiddens = run_placement_batch(p_cnn, p_rnn, p_params, None,
+            p_loss, p_acc, hiddens = run_placement_batch(p_cnn, p_rnn, None,
                 p_criterion, batch, device, writer, do_train=False)
 
             total_p_loss += p_loss
@@ -408,8 +408,8 @@ def run_models(train_iter, valid_iter, test_iter, num_epochs, dataset_type, devi
 
         for i, batch in enumerate(train_iter):
             placement_loss, placement_acc, clstm_hiddens = run_placement_batch(placement_cnn, 
-                placement_rnn, placement_params, placement_optim, PLACEMENT_CRITERION,
-                batch, device, writer, do_train=True, curr_train_epoch=epoch)
+                placement_rnn, placement_optim, PLACEMENT_CRITERION, batch, device, 
+                writer, do_train=True, curr_train_epoch=epoch)
 
             selection_loss, selection_acc = run_selection_batch(selection_rnn, selection_optim,
                 SELECTION_CRITERION, batch, device, clstm_hiddens, do_train=True)
@@ -432,8 +432,8 @@ def run_models(train_iter, valid_iter, test_iter, num_epochs, dataset_type, devi
 
         if epoch % validate_every_x_epoch == 0:
             p_v_loss, p_v_acc, s_v_loss, s_v_acc = evaluate(placement_cnn, placement_rnn, 
-                placement_params, selection_rnn, valid_iter, PLACEMENT_CRITERION, 
-                SELECTION_CRITERION, device, writer, epoch / validate_every_x_epoch)
+                selection_rnn, valid_iter, PLACEMENT_CRITERION, SELECTION_CRITERION,
+                device, writer, epoch / validate_every_x_epoch)
 
             # track best performing model(s)
             if early_stopping:
@@ -444,8 +444,7 @@ def run_models(train_iter, valid_iter, test_iter, num_epochs, dataset_type, devi
                         save_dir, best=True)
                 else:
                     print("Validation loss increased. Stopping early..")
-                    writer.close()
-                    breakpoint
+                    break
         
         if epoch == num_epochs - 1:
             save_checkpoint(placement_cnn, placement_rnn, selection_rnn,
@@ -489,8 +488,8 @@ def main():
     valid_iter = DataLoader(valid_data, batch_size=BATCH_SIZE, collate_fn=collate_charts, shuffle=True)
     test_iter = DataLoader(test_data, batch_size=BATCH_SIZE, collate_fn=collate_charts, shuffle=True)
 
-    datasets_size_str = f"""Total charts in dataset: {len(dataset)}; Train: {len(train_data)}, 
-                            Valid: {len(valid_data)}, Test: {len(test_data)}"""
+    datasets_size_str = (f'Total charts in dataset: {len(dataset)}; Train: {len(train_data)}, 
+                           Valid: {len(valid_data)}, Test: {len(test_data)}')
     print(datasets_size_str)
 
     run_models(train_iter, valid_iter, test_iter, NUM_EPOCHS, dataset_type, device, 