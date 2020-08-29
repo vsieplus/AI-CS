7b48dcf3212691ccba1795b8f9e704f95782faae
vsieplus <ryansie@hotmail.com>
2020-08-29 01:31:27 -0700

    add peak picking plot vis.

--- shiny/generate.R
+++ shiny/generate.R
@@ -11,30 +11,6 @@ generate <- import_from_path('generate', path = file.path('..', 'generate'))
 
 CHART_LEVELS <- list('single' = 26, 'double' = 28)
 
-# get a list of directories in root/, and their respective paths
-getModelDirs <- function(root) {
-  dirnames <- list.dirs(root, full.names  =  FALSE, recursive = FALSE)
-  dirpaths <- list.dirs(root, full.names  =  TRUE, recursive = FALSE)
-  names(dirpaths) = dirnames
-  
-  dirpaths
-}
-
-# you can change modelsDir (relative path) to another folder containing
-# your models when running locally; should have structure
-# models/
-#   single/
-#     ...
-#   double/
-#     model1/
-#       model.bin (the model file(s) itself saved from training)
-#       summary.json (produced at end of training)
-#     ...
-modelsDir <- file.path('..', 'train', 'models')
-#modelsDir <- 'models'
-modelTypes <- c('single', 'double')
-modelsList <- lapply(sapply(modelTypes, function(x) file.path(modelsDir, x)), getModelDirs)
-
 # retrieve text output containing a summary of the given model
 getModelSummary <- function(modelPath, as_str) {
   modelJsonPath <- file.path(modelPath, 'summary.json')
@@ -45,22 +21,22 @@ getModelSummary <- function(modelPath, as_str) {
       modelSummary <- rjson::fromJSON(file = modelJsonPath)
 
       if(as_str) {
-        paste(sep = '\n', sprintf('Model type: %s', modelSummary[['type']]),
+        paste(sep = '<br/>', sprintf('Model type: %s', modelSummary[['type']]),
               sprintf('Epochs trained: %0.f', modelSummary[['epochs_trained']]),
               sprintf('Batch size: %0.f', modelSummary[['batch_size']]),
               sprintf('Hidden size: %0.f', modelSummary[['hidden_size']]),
-              sprintf('Selection hidden weight: %0.f', modelSummary[['selection_hidden_wt']]),
+              sprintf('Selection hidden weight: %0.2f', modelSummary[['selection_hidden_wt']]),
               sprintf('Dataset name: %s', modelSummary[['name']]),
               sprintf('Song types: %s', cat(modelSummary[['song_types']])),
               sprintf('Chart type: %s', modelSummary[['chart_type']]),
               sprintf('Chart permutations: %s', cat(modelSummary[['permutations']])),
               sprintf('Total unique songs: %0.f', modelSummary[['unique_songs']]),
-              sprintf('Total audio hours: %0.f', modelSummary[['audio_hours']]),
+              sprintf('Total audio hours: %0.4f', modelSummary[['audio_hours']]),
               sprintf('Total unique charts: %0.f', modelSummary[['unique_charts']]),
               sprintf('Minimum chart level: %0.f', modelSummary[['min_level']]),
               sprintf('Maximum chart level: %0.f', modelSummary[['max_level']]),
               sprintf('Total chart steps: %0.f', modelSummary[['total_steps']]),
-              sprintf('Avg. steps per second: %4.f', modelSummary[['avg_steps_per_second']]))
+              sprintf('Avg. steps per second: %0.4f', modelSummary[['avg_steps_per_second']]))
       } else {
         modelSummary
       }
@@ -100,20 +76,23 @@ generateChart <- function(audioPath, modelPath, level, chartType, title, artist,
 # save charts to (temp. path) 'file'
 saveCharts <- function(file) {
   # can access reactive 'chartData' list in here
+  if(is.null(chartData)) {
+    return(NULL)
+  }
   
   # use temp directory before zipping file
   origDir = setwd(tempdir())
   on.exit(setwd(origDir))
   
-  if(length(chartData[['saveFormats']]) > 1) {
+  if(length(chartData()[['saveFormats']]) > 1) {
     saveFormat = 'both'
   } else {
-    saveFormat = chartData[['saveFormats']][1]
+    saveFormat = chartData()[['saveFormats']][1]
   }
   
-  generate$save_chart(chartData[['notes']], chartData[['chartType']], chartData[['level']],
-                      saveFormat, chartData[['title']], chartData[['artist']],
-                      chartData[['audio_path']], chartData[['name']], '.')
+  generate$save_chart(chartData()[['notes']], chartData()[['chartType']], chartData()[['level']],
+                      saveFormat, chartData()[['title']], chartData()[['artist']],
+                      chartData()[['audio_path']], chartData()[['name']], '.')
   
   zip(file, list.files('.'))  
 }
